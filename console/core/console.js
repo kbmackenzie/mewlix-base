e463beb76e7bb9a663ee0bb96be8299ea405bf5f
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-24 06:10:39 -0300

    feat: Core functionality rewritten entirely in Javascript; settings are now working

--- console/core/console.js
+++ console/core/console.js
@@ -1,36 +1,69 @@
 'use strict';
 
-// A custom event for when a user sends input to the console.
+/* -------------------------------------
+ * Constants:
+ * ------------------------------------- */
+/* A custom event for when a user sends input to the console. */
 const inputReceived = new Event('input-received');
 
-const consoleInput = document.getElementById('input-box');
-const consoleLines = document.getElementById('console-lines');
+/* The console prompt string: */
+const promptMessage = '=^-x-^= $';
 
-const addLine = (str, fromUser) => {
-  const message = fromUser ? str : `=^-x-^= \$ ${str}`
+/* Console: */
+const consoleBox = document.getElementById('console');
+const input = document.getElementById('input-box');
+const lines = document.getElementById('console-lines');
+const inputButton = document.getElementById('input-button');
+const showSettings = document.getElementById('show-settings');
 
+/* Settings menu : */
+const settings = document.getElementById('settings');
+const settingsExit = document.getElementById('settings-menu-exit');
+
+/* Settings options: */
+const promptColor = document.getElementById('select-color');
+const hidePrompt = document.getElementById('hide-prompt');
+const showOutline = document.getElementById('select-outline');
+const consoleOpacity = document.getElementById('select-opacity');
+
+/* -------------------------------------
+ * Console functionality:
+ * ------------------------------------- */
+const createPrompt = () => {
+  const span = document.createElement('span');
+  span.style.color = promptColor.value;
+  span.classList.add('console__prompt');
+  span.appendChild(document.createTextNode(promptMessage));
+  return span;
+}
+
+const addLine = (text, fromUser = true) => {
   const line = document.createElement('li');
-  line.appendChild(document.createTextNode(message));
-  consoleLines.appendChild(line);
+  if (fromUser && !hidePrompt.checked) {
+    line.appendChild(createPrompt());
+  }
+  line.appendChild(document.createTextNode(text));
+
+  lines.appendChild(line);
   fixScroll();
 }
 
-function fixScroll() {
-  const parent = consoleLines.parentNode;
+const fixScroll = () => {
+  const parent = lines.parentNode;
   parent.scrollTop = parent.scrollHeight;
 }
 
-const getInput = function getInput() {
-  consoleInput.disabled = false;
-  consoleInput.focus();
+const getInput = () => {
+  input.disabled = false;
+  input.focus();
 
   return new Promise(resolve => {
-    consoleInput.addEventListener(
+    input.addEventListener(
       'input-received',
       () => {
-        const text = consoleInput.value;
-        consoleInput.value = '';
-        consoleInput.disabled = true;
+        const text = input.value;
+        input.value = '';
+        input.disabled = true;
 
         addLine(text);
         resolve(text);
@@ -40,13 +73,42 @@ const getInput = function getInput() {
   });
 }
 
-function clearConsole() {
-  consoleLines.replaceChildren();
+const clearConsole = () => {
+  lines.replaceChildren();
 }
 
-consoleInput.addEventListener('keyup', event => {
-  if (event.key !== 'Enter' || consoleInput.value === '') return;
-  consoleInput.dispatchEvent(inputReceived);
+/* -------------------------------------
+ * Events:
+ * ------------------------------------- */
+input.addEventListener('keyup', event => {
+  if (event.key !== 'Enter' || input.value === '') return;
+  input.dispatchEvent(inputReceived);
+});
+
+inputButton.addEventListener('click', () => {
+  if (input.value === '') return;
+  input.dispatchEvent(inputReceived);
+});
+
+showSettings.addEventListener('click', () => {
+  settings.classList.remove('hide-item');
+});
+
+settingsExit.addEventListener('click', () => {
+  settings.classList.add('hide-item');
+});
+
+consoleOpacity.addEventListener('change', () => {
+  consoleBox.style.opacity = `${consoleOpacity.value}%`;
+});
+
+showOutline.addEventListener('change', () => {
+  if(showOutline.checked) {
+    inputBox.classList.add('console__input--outline');
+  }
+  else {
+    inputBox.classList.remove('console__input--outline');
+  }
 });
 
 // Temporarily adding to global scope for easier debugging.
