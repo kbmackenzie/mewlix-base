0a0474dce378ab6f53f44aa7c27595a935088003
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-24 17:07:03 -0300

    feat: Added 'std.console' library to the Mewlix object; finished up console API

--- console/core/console.js
+++ console/core/console.js
@@ -83,15 +83,15 @@ const clearConsole = () => {
   lines.replaceChildren();
 };
 
-const toggleHighlight = () => {
-  if (showHighlight.checked)
+const toggleHighlight = highlight => {
+  if (highlight)
     input.classList.add('console__input--highlight');
   else
     input.classList.remove('console__input--highlight');
 };
 
-const setOpacity = () => {
-  consoleBox.style.opacity = `${consoleOpacity.value}%`;
+const setOpacity = value => {
+  consoleBox.style.opacity = `${value}%`;
 };
 
 const setProjectName = name => {
@@ -132,8 +132,13 @@ exitSettings.addEventListener('click', () => {
   settings.classList.add('hide');
 });
 
-consoleOpacity.addEventListener('change', setOpacity);
-showHighlight.addEventListener('change', toggleHighlight);
+consoleOpacity.addEventListener('change', () => {
+  setOpacity(consoleOpacity.value);
+});
+
+showHighlight.addEventListener('change', () => {
+  toggleHighlight(showHighlight.checked);
+});
 
 selectBackground.addEventListener('change', () => {
   if (selectBackground.files.length === 0) return;
@@ -143,13 +148,51 @@ selectBackground.addEventListener('change', () => {
 /* -------------------------------------
  * Initialize:
  * ------------------------------------- */
-setOpacity();
-toggleHighlight();
+setOpacity(consoleOpacity.value);
+toggleHighlight(showHighlight.checked);
 
-// Temporarily adding to global scope for easier debugging.
-// Todo: Comment these out.
-window.mewlixInput = getInput;
-window.mewlixClear = clearConsole;
-window.mewlixProject = setProjectName;
+/* -------------------------------------
+ * Standard library:
+ * ------------------------------------- */
+Mewlix.meow = async message => {
+  addLine(Mewlix.purrify(message), false);
+};
 
-export { addLine, fixScroll, getInput, clearConsole };
+Mewlix.listen = async question => {
+  if (!Mewlix.isNothing(question)) {
+    addLine(Mewlix.purrify(question), false);
+  }
+  return await getInput();
+};
+
+const ensure = Mewlix.ensure;
+
+Mewlix.Console = Mewlix.library('std.console', {
+  clear: clearConsole,
+  name: name => {
+    ensure.string(name);
+    setProjectName(name);
+  },
+  background: async path => {
+    ensure.string(path);
+    await setBackground(path);
+  },
+  opacity: value => {
+    const clamped = Mewlix.clamp(value, 0, 100);
+    consoleOpacity.value = clamped.toString();
+    setOpacity(clamped);
+    return clamped;
+  },
+  highlight: enable => {
+    showHighlight.checked = enable;
+    toggleHighlight(enable);
+  },
+  prompt_color: color => {
+    ensure.string(color);
+    promptColor.value = color;
+  },
+  hide_prompt: hide => {
+    ensure.boolean(hide);
+    hidePrompt.checked = hide;
+  },
+});
