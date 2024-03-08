7eb0dc8f06387a1bab0a1de8d11f247d32d44be5
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-08 01:20:33 -0300

    chore: added favicon line to the package.sh script

--- package.sh
+++ package.sh
@@ -16,6 +16,10 @@ for TEMPLATE in $TEMPLATES; do
     cp -n './mewlix.js' "$BUILD_FOLDER/core/mewlix.js"
     cp -n './run-mewlix.js' "$BUILD_FOLDER/core/run-mewlix.js"
 
+    if [ "$TEMPLATE" != 'library' ]; then
+        cp -n './favicon.svg' "$BUILD_FOLDER/favicon.svg"
+    fi
+
     echo "Zipping '$TEMPLATE':"
     cd "$BUILD_FOLDER"
     zip -r "../$TEMPLATE" .
