3256b54150e68f42be8ad59efc0bba0de32dda9a
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-10 01:04:59 -0300

    refactor: re-organized Mewlix.Base definition

--- mewlix.js
+++ mewlix.js
@@ -843,86 +843,6 @@ Mewlix.Base = Mewlix.library('std', {
       `std.reverse: Can't check emptiness of value of type "${typeOfValue}": ${value}`);
   },
 
-  /* Converts a value to a number.
-   * type: ((number | string | boolean)) -> number */
-  slap: function slap(value) {
-    return Mewlix.Conversion.toNumber(value);
-  },
-
-  /* Rounds a number to its nearest integer.
-   * type: (number) -> number */
-  round: function round(value) {
-    ensure.number(value);
-    return Math.round(value);
-  },
-
-  /* Rounds down a number.
-   * type: (number) -> number */
-  floor: function floor(value) {
-    ensure.number(value);
-    return Math.floor(value);
-  },
-
-  /* Rounds up a number.
-   * type: (number) -> number */
-  ceiling: function ceiling(value) {
-    ensure.number(value);
-    return Math.ceil(value);
-  },
-
-  /* Clamps a number to a minimum and a maximum value.
-   * type: (number, number, number) -> number */
-  clamp: function clamp(value, min, max) {
-    ensure.all.number(value, min, max);
-    return Mewlix.clamp(value, min, max);
-  },
-
-  /* Generates a random number x between 0 and 1, where 0 <= x < 1.
-   * type: () -> number */
-  random: function random() {
-    return Math.random();
-  },
-
-  /* Generates a random integer number between min and max, where min <= x <= max.
-   * Thus, 'min' and 'max' are inclusive.
-   * If only one argument 'n' is passed, this function generates a number between 0 and n.
-   * type: (number, number) -> number */
-  random_int: function random_int(min, max) {
-    if (max === undefined) {
-      max = min;
-      min = 0;
-    }
-    ensure.all.number(min, max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-
-  /* Akin to python's range(). It aims be a little smart:
-   * count(3) should give you 0, 1, 2, 3.
-   * count(1, 3) should give you 1, 2, 3.
-   * count(3, 1) should give you 3, 2, 1.
-   *
-   * type: (number, number?) -> shelf */
-  count: function count(start = 0, end) {
-    if (end === undefined) {
-      end = start;
-      start = 0;
-    }
-    ensure.all.number(start, end);
-
-    const array = [];
-    if (start < end) {
-      for (let count = start; count <= end; count++) {
-        array.push(count);
-      }
-    }
-    else {
-      for (let count = start; count >= end; count--) {
-        array.push(count);
-      }
-    }
-    return Mewlix.Shelf.fromArray(array);
-  },
-
   /* Applies a function to each item in the shelf, returning a new shelf.
    * type: (shelf, (any) -> any) -> shelf */
   map: function map(shelf, callback) {
@@ -1038,6 +958,86 @@ Mewlix.Base = Mewlix.library('std', {
       }],
     ]);
     return box;
+  },
+
+  /* Converts a value to a number.
+   * type: ((number | string | boolean)) -> number */
+  slap: function slap(value) {
+    return Mewlix.Conversion.toNumber(value);
+  },
+
+  /* Rounds a number to its nearest integer.
+   * type: (number) -> number */
+  round: function round(value) {
+    ensure.number(value);
+    return Math.round(value);
+  },
+
+  /* Rounds down a number.
+   * type: (number) -> number */
+  floor: function floor(value) {
+    ensure.number(value);
+    return Math.floor(value);
+  },
+
+  /* Rounds up a number.
+   * type: (number) -> number */
+  ceiling: function ceiling(value) {
+    ensure.number(value);
+    return Math.ceil(value);
+  },
+
+  /* Clamps a number to a minimum and a maximum value.
+   * type: (number, number, number) -> number */
+  clamp: function clamp(value, min, max) {
+    ensure.all.number(value, min, max);
+    return Mewlix.clamp(value, min, max);
+  },
+
+  /* Generates a random number x between 0 and 1, where 0 <= x < 1.
+   * type: () -> number */
+  random: function random() {
+    return Math.random();
+  },
+
+  /* Generates a random integer number between min and max, where min <= x <= max.
+   * Thus, 'min' and 'max' are inclusive.
+   * If only one argument 'n' is passed, this function generates a number between 0 and n.
+   * type: (number, number) -> number */
+  random_int: function random_int(min, max) {
+    if (max === undefined) {
+      max = min;
+      min = 0;
+    }
+    ensure.all.number(min, max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  /* Akin to python's range(). It aims be a little smart:
+   * count(3) should give you 0, 1, 2, 3.
+   * count(1, 3) should give you 1, 2, 3.
+   * count(3, 1) should give you 3, 2, 1.
+   *
+   * type: (number, number?) -> shelf */
+  count: function count(start = 0, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    ensure.all.number(start, end);
+
+    const array = [];
+    if (start < end) {
+      for (let count = start; count <= end; count++) {
+        array.push(count);
+      }
+    }
+    else {
+      for (let count = start; count >= end; count--) {
+        array.push(count);
+      }
+    }
+    return Mewlix.Shelf.fromArray(array);
   },
 });
 
