4f001445dccd7eee4c5f1471a5bb1dadd112b827
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-22 07:56:39 -0300

    chore: Added comment headers in every core .js file

--- run-mewlix.js
+++ run-mewlix.js
@@ -1,3 +1,13 @@
+/* Mewlix is a cat-themed esoteric programming language. 🐱
+ * THis is a core file from Mewlix's base library.
+ * 
+ * Learn more at:
+ * > https://github.com/KBMackenzie/mewlix <
+ *
+ * This file is released under the MIT license. 
+ * The full license details can be found at:
+ * > https://github.com/KBMackenzie/mewlix-base/blob/main/LICENSE < */
+
 'use strict';
 
 const scriptList = () => fetch('/core/script-list.json')
