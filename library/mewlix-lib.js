125e2b3f098a8414808996d1be6ea4b648cd38a5
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-25 05:55:05 -0300

    fix(library): When importing modules, make sure to use proper relative paths

--- library/mewlix-lib.js
+++ library/mewlix-lib.js
@@ -3,7 +3,7 @@
 export default async (options = {}) => {
   const { before, meow, listen, cleanUp } = options;
 
-  await import('core/mewlix.js');
+  await import('./core/mewlix.js');
   if (meow)   { Mewlix.meow   = meow;   }
   if (listen) { Mewlix.listen = listen; }
   await before?.();
@@ -12,5 +12,5 @@ export default async (options = {}) => {
     await f();
     if (cleanUp) { delete globalThis.Mewlix; }
   };
-  await import('core/run-mewlix.js');
+  await import('./core/run-mewlix.js');
 };
