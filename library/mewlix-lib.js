829842b586be850ce8303b01c2e1b4bf5039670b
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-25 05:26:29 -0300

    feat(library): Added mewlix-lib.js

---
+++ library/mewlix-lib.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export default async (options = {}) => {
+  const { before, meow, listen, cleanUp } = options;
+
+  await import('core/mewlix.js');
+  if (meow)   { Mewlix.meow   = meow;   }
+  if (listen) { Mewlix.listen = listen; }
+  await before?.();
+
+  Mewlix.run = async f => {
+    await f();
+    if (cleanUp) { delete globalThis.Mewlix; }
+  };
+  await import('core/run-mewlix.js');
+};
