3c51ffa500fa079943d6fcfd968f958469065619
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-27 17:37:57 -0300

    feat: Added 'loadScript' util to run-mewlix

--- run-mewlix.js
+++ run-mewlix.js
@@ -13,6 +13,18 @@
 const scriptList = () => fetch('./core/script-list.json')
   .then(response => response.json());
 
+const loadScript = function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src  = src;
+    script.addEventListener('load', resolve);
+    script.addEventListener('error', reject);
+
+    document.body.appendChild(script);
+  });
+};
+
 const setTitle = name => {
   if (!name) return;
   document.title = name;
@@ -24,7 +36,7 @@ export default async () => {
   const entrypoint = data.entrypoint || 'main';
   
   for (const script of data.scripts) {
-    await import(script);
+    await loadScript(script);
   }
   await Mewlix.run(() => Mewlix.Modules.getModule(entrypoint));
 };
