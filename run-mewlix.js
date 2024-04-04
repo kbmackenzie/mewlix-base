8a686338d91f57bf5f1ad0e7b91eed380f3c05ca
kbmackenzie <kelly.a.betty@gmail.com>
2024-04-04 18:08:46 -0300

    style: loadScript should be an arrow function

--- run-mewlix.js
+++ run-mewlix.js
@@ -13,17 +13,15 @@
 const scriptList = () => fetch('./core/script-list.json')
   .then(response => response.json());
 
-const loadScript = function loadScript(src) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src  = src;
-    script.addEventListener('load', resolve);
-    script.addEventListener('error', reject);
+const loadScript = src => new Promise((resolve, reject) => {
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.src  = src;
+  script.addEventListener('load', resolve);
+  script.addEventListener('error', reject);
 
-    document.body.appendChild(script);
-  });
-};
+  document.body.appendChild(script);
+});
 
 const setTitle = name => {
   if (!name) return;
