e647740d849c18f619b8011c7355b2a4450d8590
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-13 08:23:19 -0300

    chore(build): Add build.sh script

---
+++ build.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+rm -rf './build'
+mkdir  './build'
+
+COMPILED='./build/compiled'
+FINAL='./build/final'
+
+mkdir "$COMPILED"
+mkdir "$FINAL"
+
+HEADER='./header.js'
+TERSER_CONFIG='./terser.config.json'
+
+# Write an error message to stderr.
+print_error() {
+  echo "$1" 1>&2
+}
+
+# Compile all .ts files in ./src/
+compile() {
+  npx tsc --outDir "$COMPILED"
+}
+
+# Minify a .js file (writes output to stdout)
+minify() {
+  npx terser "$1" --config-file "$TERSER_CONFIG"
+}
+
+# Compile and minify all files, and prepend comment header to them.
+build() {
+  compile || {
+    print_error "Error when compiling .ts files!"
+    exit 1
+  }
+  for FILE in "$COMPILED"/*.js; do
+    NAME=$(basename "$FILE")
+    minify "$FILE" | cat "$HEADER" - > "$FINAL/$NAME" || {
+      print_error "Error when minifying file '$FILE'!"
+      exit 1
+    }
+  done
+}
+
+# Package template (after building):
+package_template() {
+  echo "Packaging '$1' template:"
+  TEMPLATE="./templates/$1"
+  TARGET_FOLDER="./build/$1-build"
+
+  if [ ! -d "$TEMPLATE" ]; then
+    print_error "Invalid template: $1"
+    exit 1
+  fi
+  cp -r "$TEMPLATE" "$TARGET_FOLDER"
+
+  mkdir "$TARGET_FOLDER/core"
+  cp "$FINAL/mewlix.js" "$TARGET_FOLDER/core"
+  cp "$FINAL/$1.js"     "$TARGET_FOLDER/core"
+
+  echo "Zipping '$1' template:"
+  zip -r "./build/$1" "$TARGET_FOLDER"/* || {
+    print_error "Couldn't zip '$1' template!"
+    exit 1
+  }
+}
+
+# Package all templates:
+package_all() {
+  package_template 'console'
+  package_template 'graphic'
+  package_template 'library'
+}
