a319830623b3dda54bc8fdb4e41c7624290c0eff
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-13 02:20:34 -0300

    feat(meta): Write better meta header comment

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 /* Mewlix is a cat-themed esoteric programming language. 🐱
  * THis is a core file from Mewlix's base library.
- * 
- * Learn more at:
- * > https://github.com/kbmackenzie/mewlix <
+ * Learn more at: https://github.com/kbmackenzie/mewlix
  *
- * (c) kbmackenzie 2024. Released under the MIT License.
+ * Copyright (c) kbmackenzie 2024
+ * Released under the MIT License.
+ *
  * The full license details can be found at:
- * > https://github.com/kbmackenzie/mewlix-base/blob/main/LICENSE < */
+ * https://github.com/kbmackenzie/mewlix-base/blob/main/LICENSE */
