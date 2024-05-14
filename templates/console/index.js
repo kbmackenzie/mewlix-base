2d984e2420b016c5786f12ae2ea245893430b54c
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-14 06:20:15 -0300

    feat(console): Update index.js script

--- templates/console/index.js
+++ templates/console/index.js
@@ -7,8 +7,11 @@ export default async function() {
   const readMeta = () => fetch('./core/meta.json')
     .then(response => response.json());
 
-  globalThis.Mewlix = createMewlix(),
-  initConsole(),
+  globalThis.Mewlix = createMewlix();
+  const [consoleLib, consoleLibCurry] = initConsole();
+  Mewlix.Console = consoleLib;
+  Mewlix.ConsoleCurry = consoleLibCurry;
+
   initYarnball();
 
   const meta = await readMeta();
