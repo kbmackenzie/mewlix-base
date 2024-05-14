35b04c0ce4ce8afe1ec5401eeb7e05e3da9af7af
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-14 05:31:16 -0300

    feat(console): Update index.js script

--- templates/console/index.js
+++ templates/console/index.js
@@ -1,5 +1,5 @@
 'use strict';
-import initBase from './core/mewlix.js';
+import createMewlix from './core/mewlix.js';
 import initConsole from './core/console.js'
 import initYarnball from './yarnball/yarnball.js'
 
@@ -7,11 +7,11 @@ export default async function() {
   const readMeta = () => fetch('./core/meta.json')
     .then(response => response.json());
 
-  const meta = await readMeta();
-  globalThis.Mewlix = {};
-  initBase(),
+  globalThis.Mewlix = createMewlix(),
   initConsole(),
   initYarnball();
+
+  const meta = await readMeta();
   if (meta.name) {
     document.title = meta.name;
   }
