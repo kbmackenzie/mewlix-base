9bdbf9a8322d62706a68ff4a62a237b6d421229e
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-17 02:36:04 -0300

    feat(console): Update index.js script, use local state

--- templates/console/index.js
+++ templates/console/index.js
@@ -3,22 +3,21 @@ import createMewlix from './core/mewlix.js';
 import initConsole from './core/console.js'
 import initYarnball from './yarnball/yarnball.js'
 
-export default async function() {
+export default async function(callback) {
   const readMeta = () => fetch('./core/meta.json')
     .then(response => response.json());
 
-  globalThis.Mewlix = createMewlix();
-  const [consoleLib, consoleLibCurry] = initConsole();
-  Mewlix.Console = consoleLib;
-  Mewlix.ConsoleCurry = consoleLibCurry;
-
-  initYarnball();
+  const mewlix = createMewlix();
+  initConsole(mewlix);
+  if (callback) {
+    callback(mewlix);
+  }
+  initYarnball(mewlix);
 
   const meta = await readMeta();
   if (meta.name) {
     document.title = meta.name;
   }
   const entrypoint = meta.entrypoint || 'main';
-
-  return Mewlix.run(() => Mewlix.Modules.getModule(entrypoint));
+  return mewlix.run(() => mewlix.Modules.getModule(entrypoint));
 }
