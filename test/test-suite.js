8b999f0baa86e7a346562f301345d8387ccb60e7
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-20 04:23:04 -0300

    test: Keep track of errors in TestSuite; add logging util

--- test/test-suite.js
+++ test/test-suite.js
@@ -1,11 +1,21 @@
 class TestSuite {
   constructor() {
-    this.tests = [];
+    this.tests  = [];
   }
   run(name, func) {
-    const value = func();
+    let result = false;
+    try {
+      result = func();
+    }
+    catch (error) {
+      this.tests.push({
+        type: 'error',
+        name: name,
+        error: error,
+      });
+    }
     this.tests.push({
-      type: value ? 'success' : 'failure',
+      type: result ? 'success' : 'failure',
       name: name,
     });
   }
@@ -14,11 +24,22 @@ class TestSuite {
       tests:   this.tests,
       success: this.tests.filter(x => x.type === 'success'),
       failure: this.tests.filter(x => x.type === 'failure'),
+      errors:  this.tests.filter(x => x.type === 'error'),
     };
   }
   message() {
     const summary = this.summary();
-    return `Passed ${summary.success.length}/${summary.tests.length} tests.`;
+    return `Passed ${summary.success.length}/${summary.tests.length} tests. `
+      + (summary.errors.length > 0 ? `Caught ${summary.errors.length} errors.` : '');
+  }
+  logFailures(log) {
+    for (const test of this.tests) {
+      if (test.type === 'success') continue;
+      const message = (test.type === 'failure')
+        ? `Failed test ${test.name}!`
+        : `Caught error in test ${test.name}: ${test.error.toString()}`;
+      log(message);
+    }
   }
 }
 
