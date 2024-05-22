31f7fe6d5f82ab1b8527a933f716c4f457c8412f
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-21 23:31:38 -0300

    test: Rewrite test suite utility for a functional approach

--- test/test-suite.js
+++ test/test-suite.js
@@ -1,46 +1,38 @@
-class TestSuite {
-  constructor() {
-    this.tests  = [];
-  }
-  run(name, func) {
-    let result = false;
+export default function testSuite(runTests) {
+  const results = [];
+
+  function run(name, fn) {
     try {
-      result = func();
+      const value = fn();
+      results.push({
+        type: value ? 'success' : 'failure',
+        name: name,
+      });
     }
     catch (error) {
-      this.tests.push({
+      results.push({
         type: 'error',
         name: name,
         error: error,
       });
     }
-    this.tests.push({
-      type: result ? 'success' : 'failure',
-      name: name,
-    });
   }
-  summary() {
-    return {
-      tests:   this.tests,
-      success: this.tests.filter(x => x.type === 'success'),
-      failure: this.tests.filter(x => x.type === 'failure'),
-      errors:  this.tests.filter(x => x.type === 'error'),
-    };
-  }
-  message() {
-    const summary = this.summary();
-    return `Passed ${summary.success.length}/${summary.tests.length} tests. `
-      + (summary.errors.length > 0 ? `Caught ${summary.errors.length} errors.` : '');
-  }
-  logFailures(log) {
-    for (const test of this.tests) {
-      if (test.type === 'success') continue;
-      const message = (test.type === 'failure')
-        ? `Failed test ${test.name}!`
-        : `Caught error in test ${test.name}: ${test.error.toString()}`;
-      log(message);
-    }
-  }
-}
 
-export default TestSuite;
+  function addMessage(messages, result) {
+    if (result.type === 'failure') {
+      messages.push(`Test "${result.name}" failed!`);
+    }
+    else if (result.type === 'error') {
+      messages.push(`Error caught in test "${result.name}": ${result.error}`);
+    }
+    return messages;
+  }
+
+  runTests(run);
+  const successCount = results.filter(x => x.type === 'success').length;
+
+  return {
+    passed: `Passed ${successCount}/${results.length} tests!`,
+    messages: results.reduce(addMessage, []),
+  };
+}
