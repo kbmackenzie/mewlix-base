785e72dcb67d4f33b2b5b1636816c48df26ea87b
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-01 04:53:24 -0300

    test: Add util for reading test config

---
+++ test/test-utils.cjs
@@ -0,0 +1,9 @@
+const fs   = require('fs/promises');
+const path = require('path');
+
+const configFile = path.resolve(__dirname, '../test-config.json');
+
+module.exports.readConfig = async function() {
+  const contents = await fs.readFile(configFile);
+  return JSON.parse(contents.toString());
+}
