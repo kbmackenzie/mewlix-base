5f30d0f0d9666e50224eca55d56940537cf4a6e6
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-04 17:22:19 -0300

    test(templates): Add tests for converting colors to hexcodes

--- test/templates/graphic.test.js
+++ test/templates/graphic.test.js
@@ -15,27 +15,52 @@ describe('mewlix graphic template', () => {
     );
   });
 
-  const colors = [
-    { hexcode: '#ffffff', values: [255, 255, 255] },
-    { hexcode: '#000000', values: [  0,   0,   0] },
-    { hexcode: '#000'   , values: [  0,   0,   0] },
-    { hexcode: '#ff0000', values: [255,   0,   0] },
-    { hexcode: '#00ff00', values: [  0, 255,   0] },
-    { hexcode: '#0000ff', values: [  0,   0, 255] },
-    { hexcode: '#f3008f', values: [243,   0, 143] },
-    { hexcode: '#9d00ff', values: [157,   0, 255] },
-    { hexcode: '#468c0f', values: [ 70, 140,  15] },
-  ];
+  describe('colors', () => {
+    const colors = [
+      { hexcode: '#ffffff', values: [255, 255, 255] },
+      { hexcode: '#000000', values: [  0,   0,   0] },
+      { hexcode: '#ff0000', values: [255,   0,   0] },
+      { hexcode: '#00ff00', values: [  0, 255,   0] },
+      { hexcode: '#0000ff', values: [  0,   0, 255] },
+      { hexcode: '#f3008f', values: [243,   0, 143] },
+      { hexcode: '#9d00ff', values: [157,   0, 255] },
+      { hexcode: '#468c0f', values: [ 70, 140,  15] },
+    ];
 
-  it.each(colors)('parses a color', async ({ hexcode, values }) => {
-    const color = await page.evaluate(
-      (hexcode) => {
-        const color = globalThis.mewlix.lib['std.graphic'].get('hex')(hexcode);
-        const { red, green, blue } = color.bindings;
-        return [red, green, blue];
-      },
-      hexcode,
-    );
-    expect(color).toStrictEqual(values);
+    it.each(colors)('parses a hexcode as a color', async ({ hexcode, values }) => {
+      const color = await page.evaluate(
+        (hexcode) => {
+          const color = mewlix.lib['std.graphic'].get('hex')(hexcode);
+          const { red, green, blue } = color.bindings;
+          return [red, green, blue];
+        },
+        hexcode,
+      );
+      expect(color).toStrictEqual(values);
+    });
+
+    it('parses a short hexcode as a color', async () => {
+      const color = await page.evaluate(
+        (hexcode) => {
+          const color = mewlix.lib['std.graphic'].get('hex')(hexcode);
+          const { red, green, blue } = color.bindings;
+          return [red, green, blue];
+        },
+        '#000',
+      );
+      expect(color).toStrictEqual([0, 0, 0]);
+    });
+
+    it.each(colors)('converts a color to a hexcode', async ({ hexcode, values }) => {
+      const hex = await page.evaluate(
+        ([r, g, b]) => {
+          const Color = mewlix.lib['std.graphic'].get('Color');
+          const color = mewlix.clowder.instantiate(Color)(r, g, b);
+          return color.get('to_hex')();
+        },
+        values
+      );
+      expect(hex).toBe(hexcode);
+    });
   });
 });
