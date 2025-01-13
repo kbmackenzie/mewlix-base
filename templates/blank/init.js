81e9d08778c533f904f3ecc5b39a69f4e30bb984
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-13 05:52:28 -0300

    feat(blank): Add template init script

---
+++ templates/blank/init.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import createMewlix from './core/mewlix.js';
+import initYarnball from './yarnball/yarnball.js';
+
+export default async function init(callback) {
+  const readMeta = () => fetch('./core/meta.json')
+    .then(response => response.json());
+
+  const mewlix = createMewlix();
+  if (callback) {
+    await callback(mewlix);
+  }
+  initYarnball(mewlix);
+
+  const meta = await readMeta();
+  const entrypoint = meta.entrypoint || 'main';
+  return mewlix.run(() => mewlix.modules.get(entrypoint));
+}
