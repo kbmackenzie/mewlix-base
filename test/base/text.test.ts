bfc238b655fabf318afa89729d3cff12fa52d65f
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-23 18:42:21 -0300

    test: Add more tests for string->byte conversion

--- test/base/text.test.ts
+++ test/base/text.test.ts
@@ -4,8 +4,10 @@ describe('text & byte sequences', () => {
   const { to_bytes: toBytes, from_bytes: fromBytes } = standardLibrary();
 
   const testInput = [
-    { bytes: [99,97,116]       , string: 'cat' },
-    { bytes: [240,159,144,177] , string: '🐱'  },
+    { bytes: [99,97,116]       , string: 'cat'  },
+    { bytes: [240,159,144,177] , string: '🐱'   },
+    { bytes: [109,101,111,119] , string: 'meow' },
+    { bytes: []                , string: ''     },
   ];
 
   test.each(testInput)('generates a string from an utf-8 byte sequence', ({ bytes, string }) => {
