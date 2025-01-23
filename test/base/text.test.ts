85a31f4e9d199aac305dc15c27195e183e14765e
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-23 03:29:27 -0300

    test: Add tests for string->bytes conversion

---
+++ test/base/text.test.ts
@@ -0,0 +1,20 @@
+import { shelf, standardLibrary } from '../../src/mewlix';
+
+describe('text & byte sequences', () => {
+  const { to_bytes: toBytes, from_bytes: fromBytes } = standardLibrary();
+
+  const testInput = [
+    { bytes: [99,97,116]       , string: 'cat' },
+    { bytes: [240,159,144,177] , string: '🐱'  },
+  ];
+
+  test.each(testInput)('generates a string from an utf-8 byte sequence', ({ bytes, string }) => {
+    const output = fromBytes(shelf.fromArray(bytes));
+    expect(output).toBe(string);
+  });
+
+  test.each(testInput)('generates an utf-8 byte sequence from a string', ({ bytes, string }) => {
+    const output = shelf.toArray(toBytes(string));
+    expect(output).toStrictEqual(bytes);
+  });
+});
