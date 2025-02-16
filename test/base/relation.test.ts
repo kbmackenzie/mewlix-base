c37e8db69d543cbc06ed42513a4aa837d38019e5
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-16 17:22:03 -0300

    test: Add comparison tests

--- test/base/relation.test.ts
+++ test/base/relation.test.ts
@@ -1,4 +1,4 @@
-import { MewlixValue, relation } from '@/mewlix';
+import { MewlixValue, Ordering, relation } from '@/mewlix';
 
 describe('equality', () => {
   type TestInput = {
@@ -23,6 +23,32 @@ describe('equality', () => {
   test.each(testInput)('should compare values for equality: %s', ({ a, b, expected }) => {
     expect(
       relation.equal(a, b)
+    ).toBe(expected);
+  });
+});
+
+describe('comparison', () => {
+  type TestInput = {
+    a: MewlixValue;
+    b: MewlixValue;
+    expected: Ordering;
+  };
+
+  const testInput: TestInput[] = [
+    { a: 413        , b: 314        , expected: Ordering.Greater },
+    { a: 413        , b: 413        , expected: Ordering.Equal   },
+    { a: 314        , b: 413        , expected: Ordering.Less    },
+    { a: 'abc'      , b: 'cde'      , expected: Ordering.Less    },
+    { a: 'abc'      , b: 'abc'      , expected: Ordering.Equal   },
+    { a: 'cde'      , b: 'abc'      , expected: Ordering.Greater },
+    { a: true       , b: false      , expected: Ordering.Greater },
+    { a: true       , b: true       , expected: Ordering.Equal   },
+    { a: false      , b: true       , expected: Ordering.Less    },
+  ];
+
+  test.each(testInput)('compares values: %s', ({ a, b, expected }) => {
+    expect(
+      relation.ordering(a, b)
     ).toBe(expected);
   });
 });
