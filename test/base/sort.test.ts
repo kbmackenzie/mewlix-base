2624699df88cf931338c264be1fb10ac95c3756b
kbmackenzie <kelly.a.betty@gmail.com>
2025-04-09 15:53:22 -0300

    test: Add more sorting tests

--- test/base/sort.test.ts
+++ test/base/sort.test.ts
@@ -3,17 +3,13 @@ import { shelf, standardLibrary } from "@/mewlix";
 describe('sort', () => {
   const { sort } = standardLibrary();
 
-  type TestInput = {
-    input   : number[];
-    expected: number[];
-  };
-
-  const inputs: TestInput[] = [
-    { input: [3, 1, 2], expected: [1, 2, 3] },
-    { input: [1, 2, 3], expected: [1, 2, 3] },
+  const inputs: ({ input: any[], expected: any[] })[] = [
+    { input: [3, 1, 2]     , expected: [1, 2, 3]      },
+    { input: [1, 2, 3]     , expected: [1, 2, 3]      },
+    { input: ['cat', 'act'], expected: ['act', 'cat'] },
   ];
 
-  test.each(inputs)('properly sorts a shelf', ({ input, expected }) => {
+  test.each(inputs)('sorts shelf created from array $input', ({ input, expected }) => {
     const output = shelf.toArray(
       sort(shelf.create(input))
     );
